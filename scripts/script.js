356986b1c525610caa337d8ced7cc57cf390c01b
wolfgangdotdev <147769021+wolfgangdotdev@users.noreply.github.com>
2023-11-16 20:17:34 +0000

    Implement Calculator

--- scripts/script.js
+++ scripts/script.js
@@ -9,27 +9,123 @@ class Calculator {
 		this.previousOperand = "";
 		this.currentOperand = "";
 		this.currentOperator = "";
-		this.updateDisplay();
-		this.currentOperandText.innerText = "0";
+		this.updateDisplay(true);
 	}
 
-	updateDisplay() {}
+	updateDisplay(resetCurrentOperand) {
+		this.previousOperandText.innerText =
+			this.previousOperand + " " + this.currentOperator;
+		this.currentOperandText.innerText = this.currentOperand;
 
-	appendNumber(number) {}
+		if (resetCurrentOperand) {
+			this.currentOperandText.innerText = "0";
+		}
+	}
 
-	chooseOperation(operator) {}
+	appendNumber(number) {
+		if (number == "." && this.currentOperand.includes(".")) return;
 
-	displayError() {}
+		this.currentOperand = this.currentOperand + number;
+		this.updateDisplay();
+	}
 
-	compute() {}
+	togglePositiveNegative() {
+		console.log("toggled");
+		if (this.currentOperand.charAt(0) == "-") {
+			this.currentOperand = this.currentOperand.slice(1);
+		} else {
+			this.currentOperand = "-" + this.currentOperand;
+		}
+		this.updateDisplay();
+	}
+
+	displayError() {
+		this.clear();
+		this.currentOperandText.innerText = "ERR";
+	}
+
+	chooseOperation(operator) {
+		if (this.currentOperand == "") return;
+
+		if (this.previousOperand != "") {
+			this.compute();
+		}
+
+		this.currentOperator = operator;
+		this.previousOperand = this.currentOperand;
+		this.currentOperand = "";
+		this.updateDisplay(true);
+	}
+
+	compute(fromEqualsButton) {
+		if (this.currentOperand == 0 && this.currentOperator == "÷") {
+			this.displayError();
+			return;
+		}
+
+		let result = "";
+		switch (this.currentOperator) {
+			case "+":
+				result = Number(this.previousOperand) + Number(this.currentOperand);
+				break;
+
+			case "-":
+				result = Number(this.previousOperand) - Number(this.currentOperand);
+				break;
+
+			case "×":
+				result = Number(this.previousOperand) * Number(this.currentOperand);
+				break;
+
+			case "÷":
+				result = Number(this.previousOperand) / Number(this.currentOperand);
+				break;
+
+			default:
+				break;
+		}
+
+		console.log(this.previousOperand);
+
+		this.previousOperand = result;
+
+		if (fromEqualsButton) {
+			this.updateDisplay();
+			this.currentOperandText.innerText = result;
+		}
+	}
 }
 
 const operatorButtons = document.querySelectorAll("[data-operator]");
 const numberButtons = document.querySelectorAll("[data-number]");
-const clearButton = document.querySelector("data-clear");
-const negativeButton = document.querySelector("data-negative");
-const equalsButton = document.querySelector("data-equals");
+const clearButton = document.querySelector("[data-clear]");
+const negativeButton = document.querySelector("[data-negative]");
+const equalsButton = document.querySelector("[data-equals]");
 const previousOperandText = document.querySelector("[data-previous-operand");
 const currentOperandText = document.querySelector("[data-current-operand");
 
 const calculator = new Calculator(previousOperandText, currentOperandText);
+
+numberButtons.forEach((button) => {
+	button.addEventListener("click", () => {
+		calculator.appendNumber(button.innerText);
+	});
+});
+
+operatorButtons.forEach((button) => {
+	button.addEventListener("click", () => {
+		calculator.chooseOperation(button.innerText);
+	});
+});
+
+clearButton.addEventListener("click", () => {
+	calculator.clear();
+});
+
+equalsButton.addEventListener("click", () => {
+	calculator.compute(true);
+});
+
+negativeButton.addEventListener("click", () => {
+	calculator.togglePositiveNegative();
+});
